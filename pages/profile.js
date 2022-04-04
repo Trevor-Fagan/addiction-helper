a049087c31228de662f77fea732a44424ac12df4
Trevor Fagan <trevorfagan77@gmail.com>
2022-04-04 16:58:16 -0400

    Updated profile page

--- pages/profile.js
+++ pages/profile.js
@@ -20,9 +20,13 @@ const profile = () => {
 
             <div className="p-20">
                 <img src={user.picture} className="rounded-full" />
-                <p>{user.name}</p>
-                <p>{user.email}</p>
-                <a href="api/auth/logout">Logout</a>
+                <p>Name: {user.name}</p>
+                <p>Email: {user.email}</p>
+                <a href="api/auth/logout">
+                    <svg style={{display: "inline-block"}} xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+                    </svg><span style={{display: "inline-block"}}>Logout</span>
+                </a>
             </div>
         </div>
         );
