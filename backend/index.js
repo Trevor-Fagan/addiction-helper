e3e630822671293536d9371a64890553730fe0ab
Trevor Fagan <trevorfagan77@gmail.com>
2022-04-04 16:46:12 -0400

    Socket is working on backend

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,23 @@
-const express = require("express");
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+    });
+  });
